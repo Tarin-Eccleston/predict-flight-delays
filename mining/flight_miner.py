112f7bad4b5175170ceb42260f30c4afd5e2f610
Tarin Eccleston <tarin.eccleston@gmail.com>
2023-04-05 17:20:48 +1200

    successfully called API. todo: make sure we are calling it correctly

--- mining/flight_miner.py
+++ mining/flight_miner.py
@@ -8,8 +8,8 @@ class FlightMiner:
     def __init__(self, username, api_key):
         self.username = username
         self.api_key = api_key
-        self.base_url = "https://flightxml.flightaware.com/json/FlightXML3/"
-        self.headers = {"Authorization": f"{self.username}:{self.api_key}"}
+        self.base_url = "https://aeroapi.flightaware.com/aeroapi/"
+        self.headers = {'x-apikey':self.api_key}
         self.flights = []
 
     def get_random_flights(self, departure_country):
@@ -22,22 +22,31 @@ class FlightMiner:
         end_date = int(today.timestamp())
 
         # Make API request for random flights
+        airport = 'KSFO'
         payload = {
-            "howMany": 1,
-            "offset": 0,
-            "filter": "ga",
-            "startDate": start_date,
-            "endDate": end_date,
-            "originCountry": departure_country
+            'max_pages': 2
+            # "howMany": 1,
+            # "offset": 0,
+            # "filter": "ga",
+            # "startDate": start_date,
+            # "endDate": end_date,
+            # "originCountry": departure_country
         }
-        response = requests.get(self.base_url + "GetHistoricalTrack", headers=self.headers, params=payload)
 
-        # Parse JSON response and extract flight data
+        # response = requests.get(self.base_url + "GetHistoricalTrack", params=payload, headers=self.headers)
+        response = requests.get(self.base_url + f"airports/{airport}/flights", params=payload, headers=self.headers)
+
         if response.status_code == 200:
-            data = json.loads(response.content.decode("utf-8"))
-            for flight in data["data"]:
-                self.flights.append(flight)
+            print(response.json())
         else:
-            print("Error retrieving flight data")
+            print("Error executing request")
+
+        # # Parse JSON response and extract flight data
+        # if response.status_code == 200:
+        #     data = json.loads(response.content.decode("utf-8"))
+        #     for flight in data["data"]:
+        #         self.flights.append(flight)
+        # else:
+        #     print("Error retrieving flight data")
 
         return self.flights
