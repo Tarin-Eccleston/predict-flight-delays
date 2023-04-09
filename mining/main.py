fd0bbd1a2aca007e7b5f9616c84c207379c0b2e1
Tarin Eccleston <tarin.eccleston@gmail.com>
2023-04-09 18:19:49 +1200

    api keys in .env file

--- mining/main.py
+++ mining/main.py
@@ -1,26 +1,23 @@
-from flight_miner import FlightMiner
 from weather_miner import WeatherMiner
-
-# Replace YOUR_USERNAME and YOUR_API_KEY with your actual FlightAware username and API key
-# for FlightAware
-username_fw = 'tarineccleston'
-api_key_fw = 'nSjnCeqBjSjB1aTCWTV7UE0xdkumBZ38'
-
-# for OpenWeather
-username_ow = 'tarineccleston'
-api_key_ow = 'bedf9c9a36ed35c1ac48bc3242e96be1'
+from dotenv import load_dotenv
+import os
 
 def main():
 
-    flight_mine = FlightMiner(username_fw, api_key_fw)
-    flight_mine.get_random_flights("United States")
+    # flight_mine = FlightMiner(username_fw, api_key_fw)
+    # flight_mine.get_random_flights("United States")
 
     # weather mining section
-    # airport_code = "JFK"
-    # timestamp = "1649214000"
+    airport_code = "JFK"
+    timestamp = "1649214000"
 
-    # weather_miner = WeatherMiner(username_ow, api_key_ow)
-    # weather_miner.get_weather_event(airport_code, timestamp)
+    load_dotenv()
+
+    api_username = os.getenv('API_USERNAME')
+    api_key = os.getenv('API_KEY')
+
+    weather_miner = WeatherMiner(api_username, api_key)
+    weather_miner.get_weather_event(airport_code, timestamp)
 
 if __name__ == "__main__": {
     main()
