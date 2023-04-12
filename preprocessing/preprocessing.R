6f6c8c3a38a99e6e4fc797468cf2f6614e745277
Tarin Eccleston <tarin.eccleston@gmail.com>
2023-04-12 15:49:33 +1200

    clean data further

--- preprocessing/preprocessing.R
+++ preprocessing/preprocessing.R
@@ -9,6 +9,10 @@ cancellation_codes_df = read.csv("data/cancellation_codes.csv")
 
 flights_processed_df = data.frame(flights_df)
 
+# remove columns which won't be useful for our prediction
+flights_processed_df = flights_processed_df %>%
+  select(-c("TAXI_OUT", "WHEELS_OFF", "SCHEDULED_TIME", "ELAPSED_TIME", "AIR_TIME", "DISTANCE", "WHEELS_ON", "TAXI_IN", "SCHEDULED_ARRIVAL", "ARRIVAL_TIME", "ARRIVAL_DELAY", "DIVERTED"))
+
 # join dataframes
 # join flights and airline carrier data
 colnames(airlines_df) = c("AIRLINE_CODE", "AIRLINE")
@@ -44,19 +48,21 @@ flights_processed_df = flights_processed_df %>%
   rename_with(~ paste0("DESTINATION_", .), colnames(airports_df)[2:7]) %>%
   relocate(paste0("DESTINATION_", colnames(airports_df)[2:7]), .after = "DESTINATION_AIRPORT_CODE")
 
-# convert n/as for all delay durations to 0
-flights_processed_df["AIR_SYSTEM_DELAY"][is.na(flights_processed_df["AIR_SYSTEM_DELAY"])] = 0
-flights_processed_df["SECURITY_DELAY"][is.na(flights_processed_df["SECURITY_DELAY"])] = 0
-flights_processed_df["AIRLINE_DELAY"][is.na(flights_processed_df["AIRLINE_DELAY"])] = 0
-flights_processed_df["LATE_AIRCRAFT_DELAY"][is.na(flights_processed_df["LATE_AIRCRAFT_DELAY"])] = 0
-flights_processed_df["WEATHER_DELAY"][is.na(flights_processed_df["WEATHER_DELAY"])] = 0
-
-# if flight is delayed for more than 15 minutes due to weather
+# if flight is delayed for more than 15 minutes due to weather or any other factors
+# we don't need to separate other delay types for this study
+# combine other delay times together
 flights_processed_df = flights_processed_df %>%
-  mutate(IS_SECURITY_DELAY = ifelse(SECURITY_DELAY >= 15, 1, 0)) %>%
-  mutate(IS_AIRLINE_DELAY = ifelse(AIRLINE_DELAY >= 15, 1, 0)) %>%
-  mutate(IS_LATE_AIRCRAFT_DELAY = ifelse(LATE_AIRCRAFT_DELAY >= 15, 1, 0)) %>%
-  mutate(IS_WEATHER_DELAY = ifelse(WEATHER_DELAY >= 15, 1, 0))
+  mutate(IS_OTHER_DELAY = ifelse((AIR_SYSTEM_DELAY >= 15) | (SECURITY_DELAY >= 15) | (AIRLINE_DELAY >= 15) | (LATE_AIRCRAFT_DELAY >= 15), 1, 0)) %>%
+  mutate(IS_WEATHER_DELAY = ifelse(WEATHER_DELAY >= 15, 1, 0)) %>%
+  mutate(OTHER_DELAY = AIR_SYSTEM_DELAY + SECURITY_DELAY + AIRLINE_DELAY + LATE_AIRCRAFT_DELAY) %>%
+  relocate("OTHER_DELAY", .before = "WEATHER_DELAY") %>%
+  select(-c("AIR_SYSTEM_DELAY", "SECURITY_DELAY", "AIRLINE_DELAY", "LATE_AIRCRAFT_DELAY"))
+
+# convert n/as for all delay durations to 0
+flights_processed_df["OTHER_DELAY"][is.na(flights_processed_df["OTHER_DELAY"])] = 0
+flights_processed_df["WEATHER_DELAY"][is.na(flights_processed_df["WEATHER_DELAY"])] = 0
+flights_processed_df["IS_OTHER_DELAY"][is.na(flights_processed_df["IS_OTHER_DELAY"])] = 0
+flights_processed_df["IS_WEATHER_DELAY"][is.na(flights_processed_df["IS_WEATHER_DELAY"])] = 0
 
 # convert numbers to string days of week
 day_names = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
@@ -79,19 +85,26 @@ convert_time <- function(time) {
 }
 
 # convert all hour times to HH:MM format
+# note: ignore effect of changing timezones and days for now
 flights_processed_df = flights_processed_df %>%
   mutate(SCHEDULED_DEPARTURE = sapply(SCHEDULED_DEPARTURE, convert_time)) %>%
-  mutate(DEPARTURE_TIME = sapply(DEPARTURE_TIME, convert_time)) %>%
-  mutate(WHEELS_OFF = sapply(WHEELS_OFF, convert_time)) %>%
-  mutate(WHEELS_ON = sapply(WHEELS_ON, convert_time)) %>%
-  mutate(SCHEDULED_ARRIVAL = sapply(SCHEDULED_ARRIVAL, convert_time)) %>%
-  mutate(ARRIVAL_TIME = sapply(ARRIVAL_TIME, convert_time))
+  mutate(DEPARTURE_TIME = sapply(DEPARTURE_TIME, convert_time))
 
-# note: ignore effect of changing timezones and days for now
-# we only really care about the SCHEDULED_DEPARTURE and DELAY_TIME at the moment as these will most likely be the
-# most useful time variables when making a prediction
-# convert SCHEDULED_DEPARTURE datetime object
-flights_processed_df = mutate(flights_processed_df, SCHEDULED_DEPARTURE_DATETIME = as.POSIXct(paste(YEAR, MONTH, DAY, SCHEDULED_DEPARTURE), format = "%Y %m %d %H:%M"))
+flights_processed_df = flights_processed_df %>%
+  mutate(FLIGHT_DATETIME = as.POSIXct(paste(YEAR, MONTH, DAY), format = "%Y %m %d")) %>%
+  relocate(FLIGHT_DATETIME, .before = "YEAR") %>%
+  select(-c("YEAR", "MONTH", "DAY")) %>%
+  mutate(SCHEDULED_DEPARTURE_DATETIME = as.POSIXct(paste(FLIGHT_DATETIME, SCHEDULED_DEPARTURE), format = "%Y-%m-%d %H:%M")) %>%
+  relocate(SCHEDULED_DEPARTURE_DATETIME, .after = SCHEDULED_DEPARTURE) %>%
+  mutate(SCHEDULED_DEPARTURE_DATETIME = as.POSIXct(paste(FLIGHT_DATETIME, SCHEDULED_DEPARTURE), format = "%Y-%m-%d %H:%M")) %>%
+  mutate(DEPARTURE_DATETIME = SCHEDULED_DEPARTURE_DATETIME + 60 * DEPARTURE_DELAY) %>%
+  relocate(DEPARTURE_DATETIME, .after = DEPARTURE_TIME)
+
+# create index for each flight
+flights_processed_df = flights_processed_df %>%
+  mutate(INDEX = row.names(flights_processed_df)) %>%
+  relocate(INDEX, .before = "FLIGHT_DATETIME")
 
 # save data for exploratory analysis
 save(flights_processed_df, file = "output/flights.RData")
+write.table(flights_processed_df, file = "output/flights.csv", sep = ",", row.names = FALSE)
