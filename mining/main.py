1036775b5dc3ffc8c940573586ba0312958c3e9e
Tarin Eccleston <tarin.eccleston@gmail.com>
2023-04-14 19:54:11 +1200

    successfully gather weather data

--- mining/main.py
+++ mining/main.py
@@ -20,17 +20,17 @@ def main():
     flights_df['SCHEDULED_DEPARTURE_TIMESTAMP'] = flights_df['SCHEDULED_DEPARTURE_DATETIME'].apply(lambda x: x.timestamp()).round(0).astype(int)
     
     # gather weather data as rows and continuously append
-    for i in range(1,len(flights_df)):
+    for i in range(0,len(flights_df)):
         airport_code = flights_df.loc[i,'ORIGIN_AIRPORT_CODE']
         timestamp = flights_df.loc[i,'SCHEDULED_DEPARTURE_TIMESTAMP']
 
-        weather_row = weather_miner.get_weather_event(airport_code, timestamp)
-        
-        weather_row = pd.DataFrame([weather_row])
-        weather_row.insert(0, 'INDEX', flights_df.loc[i, 'INDEX'])
+        weather_event = weather_miner.get_weather_event(airport_code, timestamp)
+
+        weather_event = pd.DataFrame([weather_event])
+        weather_event.insert(0, 'INDEX', flights_df.loc[i, 'INDEX'])
         print(i)
-        print(weather_row)
-        weather_data = pd.concat([weather_data, weather_row], ignore_index=True)
+        print(weather_event)
+        weather_data = pd.concat([weather_data, weather_event], ignore_index=True)
 
     weather_data.to_csv('output/weather_data.csv', index=False)
 
--- mining/weather_miner.py
+++ mining/weather_miner.py
@@ -18,22 +18,23 @@ class WeatherMiner:
 
         response = requests.get(f"https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={airport['lat']}&lon={airport['lon']}&dt={timestamp}&appid={self.api_key}&units={self.units}")
         if response.status_code == 200:
-            weather_row = response.json()
+            weather_event = response.json()
 
             # gather only the weather data component of the dictionary
             # weather_row = weather_row['data'][0]
 
-            weather_row_p = {
-                "temp": weather_row["data"][0].get("temp"),
-                "pressure": weather_row["data"][0].get("pressure"),
-                "humidity": weather_row["data"][0].get("humidity"),
-                "dew_point": weather_row["data"][0].get("dew_point"),
-                "clouds": weather_row["data"][0].get("clouds"),
-                "visibility": weather_row["data"][0].get("visibility"),
-                "wind_speed": weather_row["data"][0].get("wind_speed"),
+            weather_event = {
+                "temp": weather_event["data"][0].get("temp"),
+                "pressure": weather_event["data"][0].get("pressure"),
+                "humidity": weather_event["data"][0].get("humidity"),
+                "dew_point": weather_event["data"][0].get("dew_point"),
+                "clouds": weather_event["data"][0].get("clouds"),
+                "visibility": weather_event["data"][0].get("visibility"),
+                "wind_speed": weather_event["data"][0].get("wind_speed"),
+                "rainfall_1hr": weather_event["data"][0].get("rain", {}).get("1h"),
             }
 
-            return weather_row_p
+            return weather_event
         else:
             print("Error: Could not retrieve weather data")
             return {}
